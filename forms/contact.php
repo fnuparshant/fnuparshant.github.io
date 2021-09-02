a310df88b1c4241633daa4d48aa5b3cec9dae492
Fnu Parshant <55007880+fnuparshant@users.noreply.github.com>
2021-09-02 10:47:54 -0400

    adding comment

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => '262penny@gmail.com',
-    'password' => 'password',
+    'password' => 'pal19694',
     'port' => '587'
   );
 
